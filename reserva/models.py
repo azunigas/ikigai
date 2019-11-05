e71ab830d3aef9de63689265599581330139d870
Álvaro Hernán Zúñiga Suarez <exazusu@dom_bci_prod.cl>
2019-11-05 17:59:36 -0300

    agrego modelos de reserva

--- reserva/models.py
+++ reserva/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ikigai.models import Box
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Reserva(models.Model):
+    idReserva = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField()
+    fechaReserva = models.DateField()
+    idBox = models.ForeignKey(Box, on_delete=models.SET_NULL, null= True)
+    idUsuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class Horario(models.Model):
+    idHorario = models.AutoField(primary_key=True)
+    horario = models.CharField(max_length=2)
+    valor = models.IntegerField()
+
+
+class Hora(models.Model):
+    idHora = models.AutoField(primary_key=True)
+    hora = models.CharField(max_length=13)
+    idHorario = models.ForeignKey(Horario, on_delete=models.SET_NULL, null=True)
+
+
+class HorasReserva(models.Model):
+    idReserva = models.ForeignKey(Reserva, on_delete=models.CASCADE)
+    idHora = models.ForeignKey(Hora, on_delete=models.SET_NULL, null=True)
